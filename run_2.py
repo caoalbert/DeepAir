aac1d4d29185846871ce30186a9e17f913d56403
Joey <cazhiyu84@gmail.com>
2024-04-10 01:38:44 +0800

    New func: train_model_plot. Use it to output train loss and test loss while training. New file: run_2.py. An alternative of run.py.

---
+++ run_2.py
@@ -0,0 +1,78 @@
+import sys
+
+sys.path.insert(0, 'code')
+
+from dataset import AirportDataset, AirportGraph
+from model import DeepAir
+from train import train_model, train_model_plot
+import torch.nn as nn
+import torch
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+
+MODEL_PATH = 'trained_models/albert0408.pth'
+PARAMS_PATH = f'trained_models/albert0408.pth'
+
+MODEL_PATH_Joey = 'trained_models/joey0410.pth'
+PARAMS_PATH_Joey = f'trained_models/joey0410.pth'
+
+if __name__ == '__main__':
+    dataset = AirportDataset(training_start_quarter=[1, 1998],
+                             batch_length=8,
+                             n_train_samples=60,
+                             n_test_samples=1,
+                             prediction_horizon=4)
+
+    training_graph = AirportGraph(nodes=dataset.train_x_nodes,
+                                  edges=dataset.train_x_edges,
+                                  edge_attr=dataset.train_x_edges_attr,
+                                  y=dataset.train_y)
+
+    testing_graph = AirportGraph(nodes=dataset.test_x_nodes,
+                                 edges=dataset.test_x_edges,
+                                 edge_attr=dataset.test_x_edges_attr,
+                                 y=dataset.test_y)
+
+    training_graph.process()
+    testing_graph.process()
+
+    model = DeepAir(num_airports=len(dataset.qualified_ca_airports),
+                    prediction_horizon=dataset.prediction_horizon)
+    model_params = (len(dataset.qualified_ca_airports), dataset.prediction_horizon)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    loss = nn.MSELoss()
+    num_epochs = 200
+
+    train_losses, test_losses = train_model_plot(model, optimizer, loss, num_epochs, training_graph, testing_graph)
+
+    torch.save(model.state_dict(), MODEL_PATH_Joey)
+
+    with open(PARAMS_PATH, 'wb') as f:
+        pickle.dump((len(dataset.qualified_ca_airports), dataset.prediction_horizon), f)
+
+    # Plotting training and validation losses
+    plt.figure(figsize=(10, 6))
+    plt.plot(train_losses, label='Training Loss')
+    plt.plot(test_losses, label='Validation Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title('Training and Validation Loss')
+    plt.legend()
+    plt.show()
+
+    model.eval()
+
+    # Model evaluation as before
+    model.eval()
+    with torch.no_grad():
+        test_y_predicted = model(testing_graph.series).detach().numpy()
+        test_y_predicted = np.exp(test_y_predicted) - 1  # Reversing log transformation
+
+    np.save('test_y_predicted_joey.npy', test_y_predicted)
+    np.save('test_y_joey.npy', dataset.test_y)
+
+
+
+
